98fe4b164049f771244da334f6444a448fa1f9aa
meng_ding <meng.ding@turbonomic.com>
2019-10-17 21:06:57 -0400

    Remove Context struct

--- pkg/topology/supplychain.go
+++ pkg/topology/supplychain.go
@@ -25,15 +25,11 @@ type SupplyChainNode struct {
 	ConnectedConsumerTypes set.Set
 }
 
-type Context struct {
+type SupplyChainResolver struct {
 	VisitedEntityTypes set.Set
 	VisitedEntities    set.Set
 	NodeMap            map[int32]*SupplyChainNode
 	Frontier           []*Entity
-}
-
-type SupplyChainResolver struct {
-	Context *Context
 }
 
 func NewSupplyChainNode(entityType int32, depth int) *SupplyChainNode {
@@ -70,22 +66,16 @@ func (n *SupplyChainNode) printNode() {
 	log.Infof("Member count: %d", len(members))
 }
 
-func NewContext() *Context {
-	return &Context{
+func NewSupplyChainResolver() *SupplyChainResolver {
+	return &SupplyChainResolver{
 		VisitedEntityTypes: set.NewSet(),
 		VisitedEntities:    set.NewSet(),
 		NodeMap:            make(map[int32]*SupplyChainNode),
 	}
 }
 
-func NewSupplyChainResolver() *SupplyChainResolver {
-	return &SupplyChainResolver{
-		Context: NewContext(),
-	}
-}
-
 func (s *SupplyChainResolver) GetSupplyChainNodes(startingVertices []*Entity) {
-	s.Context.Frontier = startingVertices
+	s.Frontier = startingVertices
 	log.Infof("Collect supply chain providers")
 	// Collect supply chain providers
 	s.traverseSupplyChain(GetProviders, 1, 1)
@@ -97,7 +87,7 @@ func (s *SupplyChainResolver) GetSupplyChainNodes(startingVertices []*Entity) {
 			frontier = append(frontier, neighbor)
 		}
 	}
-	s.Context.Frontier = frontier
+	s.Frontier = frontier
 	s.traverseSupplyChain(GetConsumers, 0, -1)
 	s.collectNodeProviderConsumerTypes()
 	s.PrintNodeMap()
@@ -109,46 +99,46 @@ func (s *SupplyChainResolver) traverseSupplyChain(neighborFunc neighborFunc,
 	var visitedEntityTypesInThisDepth = set.NewSet()
 	// Process the current depth
 	//log.Infof("Current depth %d", currentDepth)
-	for len(s.Context.Frontier) > 0 {
+	for len(s.Frontier) > 0 {
 		// Dequeue
-		vertex := s.Context.Frontier[0]
-		s.Context.Frontier = s.Context.Frontier[1:]
-		if s.Context.VisitedEntities.Contains(vertex) {
+		vertex := s.Frontier[0]
+		s.Frontier = s.Frontier[1:]
+		if s.VisitedEntities.Contains(vertex) {
 			continue
 		}
-		s.Context.VisitedEntities.Add(vertex)
+		s.VisitedEntities.Add(vertex)
 		//log.Infof("Visiting %s", vertex.Name)
 		// Only add a node when we have not already visited an entity of the same type
-		if !s.Context.VisitedEntityTypes.Contains(vertex.EntityType) {
+		if !s.VisitedEntityTypes.Contains(vertex.EntityType) {
 			neighbors := neighborFunc(vertex)
 			for _, neighbor := range neighbors {
-				if !s.Context.VisitedEntities.Contains(neighbor) {
+				if !s.VisitedEntities.Contains(neighbor) {
 					nextFrontier = append(nextFrontier, neighbor)
 				}
 			}
-			node, found := s.Context.NodeMap[vertex.EntityType]
+			node, found := s.NodeMap[vertex.EntityType]
 			if !found {
 				entityType, _ := proto.EntityDTO_EntityType_name[vertex.EntityType]
 				log.Infof("Create a new supply chain node for %s", entityType)
 				node = NewSupplyChainNode(vertex.EntityType, currentDepth)
-				s.Context.NodeMap[vertex.EntityType] = node
+				s.NodeMap[vertex.EntityType] = node
 			}
 			//log.Infof("Adding a member %s to node type %v", vertex.Name, vertex.EntityType)
 			node.addMember(vertex)
 			visitedEntityTypesInThisDepth.Add(vertex.EntityType)
 		}
 	}
-	s.Context.VisitedEntityTypes = s.Context.VisitedEntityTypes.Union(visitedEntityTypesInThisDepth)
+	s.VisitedEntityTypes = s.VisitedEntityTypes.Union(visitedEntityTypesInThisDepth)
 	//log.Infof("Visited entity types %v", s.Context.VisitedEntityTypes)
 	// Process the next depth
 	if len(nextFrontier) > 0 {
-		s.Context.Frontier = nextFrontier
+		s.Frontier = nextFrontier
 		s.traverseSupplyChain(neighborFunc, currentDepth+increment, increment)
 	}
 }
 
 func (s *SupplyChainResolver) collectNodeProviderConsumerTypes() {
-	for _, node := range s.Context.NodeMap {
+	for _, node := range s.NodeMap {
 		for member := range node.Members.Iterator().C {
 			for _, provider := range member.(*Entity).Providers {
 				node.ConnectedProviderTypes.Add(provider.EntityType)
@@ -161,7 +151,7 @@ func (s *SupplyChainResolver) collectNodeProviderConsumerTypes() {
 }
 
 func (s *SupplyChainResolver) PrintNodeMap() {
-	for eType, node := range s.Context.NodeMap {
+	for eType, node := range s.NodeMap {
 		entityType, _ := proto.EntityDTO_EntityType_name[eType]
 		log.Infof("Entity Type: %s", entityType)
 		node.printNode()
